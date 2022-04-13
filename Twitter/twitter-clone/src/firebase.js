3d395e5b5347376a06c712daf1b6fbb854e989b5
Yash Gupta <53759576+239yash@users.noreply.github.com>
2022-04-13 08:14:34 +0530

    Check Firebase.Js for creds

--- Twitter/twitter-clone/src/firebase.js
+++ Twitter/twitter-clone/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC1lDB669jLdYHJ9eIu-Cb80c2AEAiXxDg",
+    apiKey: "-Cb80c2AEAiXxDg",
     authDomain: "wtftwitterclone.firebaseapp.com",
     databaseURL: "https://wtftwitterclone.firebaseio.com",
     projectId: "wtftwitterclone",
     storageBucket: "wtftwitterclone.appspot.com",
-    messagingSenderId: "113358064099",
-    appId: "1:113358064099:web:0797dc297d8b92746db8d6",
-    measurementId: "G-750QTV5J1V"
+    messagingSenderId: "",
+    appId: "1:11335864099:web:",
+    measurementId: "G-"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
