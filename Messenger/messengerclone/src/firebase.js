b91c8f0e1896bacf2e297787fb3d351a510b551c
Yash Gupta <53759576+239yash@users.noreply.github.com>
2022-04-13 08:13:51 +0530

    Update firebase.js

--- Messenger/messengerclone/src/firebase.js
+++ Messenger/messengerclone/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCqH82XxBJbftafq-73-sfGfy3uYFlyC7M",
+    apiKey: "-73-sfGfy3uYFlyC7M",
     authDomain: "messenger-clone-5b99e.firebaseapp.com",
     databaseURL: "https://messenger-clone-5b99e.firebaseio.com",
     projectId: "messenger-clone-5b99e",
     storageBucket: "messenger-clone-5b99e.appspot.com",
-    messagingSenderId: "298168941918",
-    appId: "1:298168941918:web:72f363912590b9348e63cf",
-    measurementId: "G-R82VTTQ8FN"
+    messagingSenderId: "",
+    appId: "1:29816894918:web:",
+    measurementId: "G-"
 });
 
 const db = firebaseApp.firestore();
