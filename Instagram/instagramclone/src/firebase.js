c6985eca387d7540ba81299bf77be5f9acf7054c
Yash Gupta <53759576+239yash@users.noreply.github.com>
2022-04-13 08:13:27 +0530

    check Firebase.Js

--- Instagram/instagramclone/src/firebase.js
+++ Instagram/instagramclone/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseapp = firebase.initializeApp({
-    apiKey: "AIzaSyBHdUXvG0bi-A2JdjD5t61GejBWOf4DPNM",
+    apiKey: "-A2JdjD5t61GejBWOf4DPNM",
     authDomain: "instagram-clone-754a9.firebaseapp.com",
     databaseURL: "https://instagram-clone-754a9.firebaseio.com",
     projectId: "instagram-clone-754a9",
     storageBucket: "instagram-clone-754a9.appspot.com",
-    messagingSenderId: "993716280059",
-    appId: "1:993716280059:web:aae6b75bde43fd4c4cee5e",
-    measurementId: "G-BZXM2K900P"
+    messagingSenderId: "",
+    appId: "1::web:aae6b75bde43fd4c4cee5e",
+    measurementId: "G-"
 });
 
 const db = firebaseapp.firestore();
